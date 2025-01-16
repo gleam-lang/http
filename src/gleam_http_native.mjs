18f985e5a2d7d533174d1265c514395e905618be
arnu515 <52203828+arnu515@users.noreply.github.com>
2025-01-16 13:21:35 +0530

    feat: dynamic method decoding

--- src/gleam_http_native.mjs
+++ src/gleam_http_native.mjs
@@ -9,29 +9,65 @@ import {
   Connect,
   Options,
   Patch,
+  Other,
 } from "./gleam/http.mjs";
+
+/**
+  @param {number} ch
+*/
+export function is_valid_tchar(ch) {
+  try {
+    switch (ch) {
+      case  '33': return true;
+      case  '35': return true;
+      case  '36': return true;
+      case  '37': return true;
+      case  '38': return true;
+      case  '39': return true;
+      case  '42': return true;
+      case  '43': return true;
+      case  '45': return true;
+      case  '46': return true;
+      case  '94': return true;
+      case  '95': return true;
+      case  '96': return true;
+      case '124': return true;
+      case '126': return true;
+    }
+    // DIGIT
+    if (ch >= 0x30 && ch <= 0x39) return true;
+    // ALPHA
+    if (ch >= 0x41 && ch <= 0x5A || ch >= 0x61 && ch <= 0x7A) return true;
+  } catch {}
+  return false;
+}
 
 export function decode_method(value) {
   try {
-    switch (value.toLowerCase()) {
-      case "get":
+    switch (value) {
+      case "GET":
         return new Ok(new Get());
-      case "post":
+      case "POST":
         return new Ok(new Post());
-      case "head":
+      case "HEAD":
         return new Ok(new Head());
-      case "put":
+      case "PUT":
         return new Ok(new Put());
-      case "delete":
+      case "DELETE":
         return new Ok(new Delete());
-      case "trace":
+      case "TRACE":
         return new Ok(new Trace());
-      case "connect":
+      case "CONNECT":
         return new Ok(new Connect());
-      case "options":
+      case "OPTIONS":
         return new Ok(new Options());
-      case "patch":
+      case "PATCH":
         return new Ok(new Patch());
+    }
+    if (typeof value === 'string') {
+      for (const v of value)
+        if (!is_valid_tchar(v)) return new Error(undefined);
+      return new Ok(new Other(value));
     }
   } catch {}
   return new Error(undefined);
