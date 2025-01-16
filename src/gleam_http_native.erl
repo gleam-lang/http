e3993b1d25f6076108c6d0530ed1e76d8a4f6a52
arnu515 <52203828+arnu515@users.noreply.github.com>
2025-01-16 13:21:35 +0530

    feat: dynamic method decoding

--- src/gleam_http_native.erl
+++ src/gleam_http_native.erl
@@ -1,17 +1,47 @@
 -module(gleam_http_native).
--export([decode_method/1]).
+-export([decode_method/1, is_valid_tchar/1]).
+
+is_valid_tchar(X) when is_integer(X) ->
+    case X of
+        $! -> true;
+        $# -> true;
+        $$ -> true;
+        $% -> true;
+        $& -> true;
+        $' -> true;
+        $* -> true;
+        $+ -> true;
+        $- -> true;
+        $. -> true;
+        $^ -> true;
+        $_ -> true;
+        $` -> true;
+        $| -> true;
+        $~ -> true;
+        % DIGIT
+        _ when X >= 16#30 andalso X =< 16#39 -> true;
+        % ALPHA
+        _ when (X >= 16#41 andalso X =< 16#5A) orelse (X >= 16#61 andalso X =< 16#7A) -> true;
+        _ -> false
+    end;
+is_valid_tchar(_) -> false.
+
+is_valid_token([]) -> false;
+is_valid_token(<<>>) -> false;
+is_valid_token(X) when is_list(X) orelse is_bitstring(X) -> do_is_valid_token(X, true);
+is_valid_token(_) -> false.
+
+do_is_valid_token(_, false) -> false;
+do_is_valid_token([H|T] = X, true) when is_list(X) -> do_is_valid_token(T, is_valid_tchar(H));
+do_is_valid_token(<<H, T/bits>> = X, true) when is_bitstring(X) -> do_is_valid_token(T, is_valid_tchar(H));
+do_is_valid_token(_, X) -> X.
+
+normalise_method(X) when is_bitstring(X) -> {ok, {other, X}};
+normalise_method(X) when is_list(X) -> {ok, {other, list_to_binary(X)}};
+normalise_method(_) -> {error, nil}.
 
 decode_method(Term) ->
     case Term of
-        "connect"     -> {ok, connect};
-        "delete"      -> {ok, delete};
-        "get"         -> {ok, get};
-        "head"        -> {ok, head};
-        "options"     -> {ok, options};
-        "patch"       -> {ok, patch};
-        "post"        -> {ok, post};
-        "put"         -> {ok, put};
-        "trace"       -> {ok, trace};
         "CONNECT"     -> {ok, connect};
         "DELETE"      -> {ok, delete};
         "GET"         -> {ok, get};
@@ -21,24 +51,6 @@ decode_method(Term) ->
         "POST"        -> {ok, post};
         "PUT"         -> {ok, put};
         "TRACE"       -> {ok, trace};
-        "Connect"     -> {ok, connect};
-        "Delete"      -> {ok, delete};
-        "Get"         -> {ok, get};
-        "Head"        -> {ok, head};
-        "Options"     -> {ok, options};
-        "Patch"       -> {ok, patch};
-        "Post"        -> {ok, post};
-        "Put"         -> {ok, put};
-        "Trace"       -> {ok, trace};
-        'connect'     -> {ok, connect};
-        'delete'      -> {ok, delete};
-        'get'         -> {ok, get};
-        'head'        -> {ok, head};
-        'options'     -> {ok, options};
-        'patch'       -> {ok, patch};
-        'post'        -> {ok, post};
-        'put'         -> {ok, put};
-        'trace'       -> {ok, trace};
         'CONNECT'     -> {ok, connect};
         'DELETE'      -> {ok, delete};
         'GET'         -> {ok, get};
@@ -48,24 +60,6 @@ decode_method(Term) ->
         'POST'        -> {ok, post};
         'PUT'         -> {ok, put};
         'TRACE'       -> {ok, trace};
-        'Connect'     -> {ok, connect};
-        'Delete'      -> {ok, delete};
-        'Get'         -> {ok, get};
-        'Head'        -> {ok, head};
-        'Options'     -> {ok, options};
-        'Patch'       -> {ok, patch};
-        'Post'        -> {ok, post};
-        'Put'         -> {ok, put};
-        'Trace'       -> {ok, trace};
-        <<"connect">> -> {ok, connect};
-        <<"delete">>  -> {ok, delete};
-        <<"get">>     -> {ok, get};
-        <<"head">>    -> {ok, head};
-        <<"options">> -> {ok, options};
-        <<"patch">>   -> {ok, patch};
-        <<"post">>    -> {ok, post};
-        <<"put">>     -> {ok, put};
-        <<"trace">>   -> {ok, trace};
         <<"CONNECT">> -> {ok, connect};
         <<"DELETE">>  -> {ok, delete};
         <<"GET">>     -> {ok, get};
@@ -75,14 +69,9 @@ decode_method(Term) ->
         <<"POST">>    -> {ok, post};
         <<"PUT">>     -> {ok, put};
         <<"TRACE">>   -> {ok, trace};
-        <<"Connect">> -> {ok, connect};
-        <<"Delete">>  -> {ok, delete};
-        <<"Get">>     -> {ok, get};
-        <<"Head">>    -> {ok, head};
-        <<"Options">> -> {ok, options};
-        <<"Patch">>   -> {ok, patch};
-        <<"Post">>    -> {ok, post};
-        <<"Put">>     -> {ok, put};
-        <<"Trace">>   -> {ok, trace};
-        _             -> {error, nil}
+        X             ->
+            case is_valid_token(X) of
+                true -> normalise_method(X);
+                false -> {error, nil}
+            end
     end.
